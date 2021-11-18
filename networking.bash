0b7f0fe7e6369b8913523761a3434e00097a8e9c
Gabriel Trabanco <1969593+gtrabanco@users.noreply.github.com>
2021-11-18 03:53:42 +0100

    [Feature] add client (#6)
    * Cleanup
    
    * Added ipv6 support
    
    * Separated ipv4 & ipv6
    
    * Script to open a port
    
    * Changed variable VPN_NETWORK_CDR for VPN_NETWORK_CIDR
    
    * Added log functions
    
    * Fix typo error in error message
    
    * Added more debug options
    
    * Fix missing file path
    
    * Rename generate_config script
    
    * Some debug functions fixes and refactor
    
    * Fix unbound variable
    
    * Added add new client script
    
    * Must be root
    
    * Fix use full named function instead the short aliased one
    
    * Fix order is important to use defined functions in library. Added error when core library not found.
    
    * Use echo instead of _w
    
    * Fix unknow error while try to print an error if a library could not be loaded
    
    * Fix typo & pre shared key
    
    * Added --peer-name & --ip
    
    * Fix set var when no value is provided
    
    * Fix not needed set of peer name that causes an error when naming
    
    Co-authored-by: Gabriel Trabanco Llano <gtrabanco@users.noreply.github.com>

--- networking.bash
+++ networking.bash
@@ -1,6 +1,104 @@
 #!/usr/bin/env bash
 #shellcheck disable=SC2206,SC2207,SC2016
 
+# Return true if it is: true, 1 yes, y, on or enable
+_check_true() {
+  [[ ${1:-} == true || ${1:-} == 1 || ${1:-} == yes || ${1:-} == y || ${1:-} == on || ${1:-} == enable ]]
+}
+
+# Return false if it is: false, 0 no, n, off or disable
+_check_false() {
+  ! [[ ${1:-} == false || ${1:-} == 0 || ${1:-} == no || ${1:-} == n || ${1:-} == off || ${1:-} == disable ]]
+}
+
+_w() {
+  echo "$*"
+}
+
+_info() {
+  _w "$*" >&2
+}
+
+_i() {
+  _info "$*"
+}
+
+_warn() {
+  _info "WARNING: $*" | _log "== FATAL ERROR =="
+  _log "WARNING: $*"
+  exit 4
+}
+
+_debug() {
+  _check_true "${DEBUG:-false}" && _info "$*"
+}
+
+_d() {
+  _debug "$*"
+}
+
+_log () {
+  if [[ $# -gt 0 ]]; then
+    printf "%s\n" "$@" | tee -a "${LOG_FILE:-${HOME}/wireguard-setup.log}" &> /dev/null
+  fi
+
+  _debug "$@"
+  
+  if [[ ! -t 0 ]]; then
+    printf "%s\n" "$(< /dev/stdin)" | tee -a "${LOG_FILE:-${HOME}/wireguard-setup.log}"
+  fi
+
+  echo | tee -a "${LOG_FILE:-${HOME}/wireguard-setup.log}" &> /dev/null
+}
+
+_log_exec() {
+  echo "$*" | _log "Executing command" &> /dev/null
+  "$@" 2>&1 | _log "Command output"
+  echo "End of command execution" | _log &> /dev/null
+}
+
+_set() {
+  local -r var_name="${1:-}"
+  [[ -z "${var_name}" ]] && _warn "No variable name provided"
+  shift
+  _debug "Setting var '${var_name}' to the value(s) '$*'"
+  if [[ $# -gt 1 ]]; then
+    eval "${var_name}=(${*})"
+  else
+    eval "${var_name}=\"${1:-\"\"}\""
+  fi
+}
+
+_s() {
+  _set "$@"
+}
+
+_set_secret() {
+  local -r var_name="${1:-}"
+  [[ -z "${var_name}" ]] && _warn "No variable name provided"
+  shift
+  _debug "Setting var '${var_name}'"
+  if [[ $# -gt 1 ]]; then
+    eval "${var_name}=(${*})"
+  else
+    eval "${var_name}=${1:-}"
+  fi
+}
+
+_ss() {
+  _set_secret "$@"
+}
+
+_unset() {
+  [[ $# -eq 0 ]] && return
+  _d "Unsetting var(s) $(printf "%s " "$@")"
+  unset "$@"
+}
+
+_u() {
+  _unset "$@"
+}
+
 start_sudo() {
   if ! has_sudo; then
     command sudo -v -B
@@ -382,6 +480,16 @@ gen_peer_config() {
 }
 
 iptables_remove_duplicates() {
+  if
+    ! command -v service &>/dev/null ||
+    ! command -v iptables &>/dev/null ||
+    ! command -v iptables-save &>/dev/null ||
+    ! command -v iptables-restore &>/dev/null
+  then
+    _info "Any iptables command does not exist or could not be found" | _log
+    return
+  fi
+
   command service iptables save &> /dev/null || true
   command iptables-save | command awk '/^COMMIT$/ { delete x; }; !x[$0]++' | tee /tmp/iptables.conf &> /dev/null
   command iptables -F
@@ -412,6 +520,7 @@ qrencode_dependency_installed() {
   fi
 }
 
+
 show_file_as_qr() {
   [[ ! -r "${1:-}" ]] && return 1
   ! qrencode_dependency_installed && return 1
@@ -423,22 +532,4 @@ generate_qr_code_from_file() {
   ! qrencode_dependency_installed && return 1
 
   qrencode -m 2 -t ansiutf8 -o "${2:-}" <<< "$1"
-}
-
-_log () {
-  if [[ $# -gt 0 ]]; then
-    printf "%s\n" "$@" | tee -a "${LOG_FILE:-${HOME}/wireguard-setup.log}" &> /dev/null
-  fi
-  
-  if [[ ! -t 0 ]]; then
-    printf "%s\n" "$(< /dev/stdin)" | tee -a "${LOG_FILE:-${HOME}/wireguard-setup.log}"
-  fi
-
-  echo | tee -a "${LOG_FILE:-${HOME}/wireguard-setup.log}" &> /dev/null
-}
-
-_log_exec() {
-  echo "$*" | _log "Executing command" &> /dev/null
-  "$@" 2>&1 | _log "Command output"
-  echo "End of command execution" | _log &> /dev/null
 }
